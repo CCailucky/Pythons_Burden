a4bb127bbbeaac5a35ac24de4cbbb7c4d7ffc999
CCailucky <1023768887@qq.com>
2026-05-04 15:14:29 +1000

    main frame

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import pygame
+
+
 def main():
-    pass
+    pygame.init()
+
+    screen = pygame.display.set_mode((750, 600)) # width 750 height 600
+    pygame.display.set_caption("Python's Burden: Escape from COMP9001")
+
+    game_running = True
+
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+
+        screen.fill((30, 30, 30))
+
+        pygame.display.flip()
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
