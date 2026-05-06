4b3a3566b85d4bd7028818140727417dbd2803a4
CCailucky <1023768887@qq.com>
2026-05-06 23:05:33 +1000

    add apple manager, change some data structure

--- apple.py
+++ apple.py
@@ -57,70 +57,124 @@ class Apple:
         return next_head == self.pos
 
 
-
-def get_random_apple_letter() -> str:
-    return random.choice(string.ascii_uppercase)  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-
-# multiple apples
-def spawn_and_get_apples(
-    snake_body: list[tuple[int, int]],
-    game_map,
-    collected_letters: list[str],
-    max_apples: int,
-) -> list[Apple]:
-    apples = []
-
-    # Store all positions that apples should not spawn on.
-    occupied_positions = snake_body.copy()
-    next_index = len(collected_letters)
-
-    if next_index < len(TARGET_SEQUENCE):
-        correct_letter = TARGET_SEQUENCE[next_index]
-    else:
-        correct_letter = "?"
-    # Make sure there is at least one correct letter apple.
-    correct_apple = Apple(
+class AppleManager:
+    def __init__(
+        self,
+        player_snake,
         game_map,
-        correct_letter,
-        occupied_positions,
-    )
-    # Make sure there is at least one correct letter apple.
-    apples.append(correct_apple)
-    occupied_positions.append(correct_apple.pos)
-    # Spawn random letter apples.
-    while len(apples) < max_apples:
-        random_letter = get_random_apple_letter()
-        random_apple = Apple(
+        collected_letters: list[str],
+        max_apples: int,
+    ):
+        self.max_apples = max_apples
+        self.apples = []
+        self.spawn_apples(player_snake, game_map, collected_letters)
+
+    def get_next_required_letter(self, collected_letters: list[str]) -> str:
+        next_index = len(collected_letters)
+        if next_index < len(TARGET_SEQUENCE):
+            return TARGET_SEQUENCE[next_index]
+        return "?"
+
+    def spawn_apples(
+        self,
+        player_snake,
+        game_map,
+        collected_letters: list[str],
+    ) -> None:
+        self.apples = []
+        # NOT AVAILABLE POSITION FOR SPAWNING
+        occupied_positions = player_snake.body.copy()
+        correct_letter = self.get_next_required_letter(collected_letters)
+        # Make sure there is at least one correct letter apple.
+        correct_apple = Apple(
             game_map,
-            random_letter,
+            correct_letter,
             occupied_positions,
         )
+        self.apples.append(correct_apple)
+        occupied_positions.append(correct_apple.pos)
+        # Spawn random letter apples.
+        while len(self.apples) < self.max_apples:
+            random_letter = get_random_apple_letter()
+            random_apple = Apple(
+                game_map,
+                random_letter,
+                occupied_positions,
+            )
+            self.apples.append(random_apple)
+            occupied_positions.append(random_apple.pos)
 
-        apples.append(random_apple)
-        occupied_positions.append(random_apple.pos)
+    def get_eaten_apple(self, next_head: tuple[int, int]) -> Apple | None:
+        for apple in self.apples:
+            if apple.check_apple_eaten(next_head):
+                return apple
+        return None
 
-    return apples
+    def handle_apple_eaten(
+        self,
+        eaten_apple: Apple,
+        player_snake,
+        game_map,
+        collected_letters: list[str],
+    ) -> list[str]:
+        collected_letters.append(eaten_apple.letter)
 
+        self.spawn_apples(
+            player_snake,
+            game_map,
+            collected_letters,
+        )
 
-def get_eaten_apple(
-    next_head: tuple[int, int],
-    apples: list[Apple],
-) -> Apple | None:
-    for apple in apples:
-        if apple.check_apple_eaten(next_head):
-            return apple
+        return collected_letters
 
-    return None
-
-
-def handle_apple_eaten(
-    eaten_apple: Apple,
-    collected_letters: list[str],
-) -> list[str]:
-    collected_letters.append(eaten_apple.letter)
-    return collected_letters
+    def draw(self, screen, font) -> None:
+        for apple in self.apples:
+            apple.draw(screen, font)
 
 
 def check_target_completed(collected_letters: list[str]) -> bool:
     return "".join(collected_letters) == TARGET_SEQUENCE
+
+
+def get_random_apple_letter() -> str:
+    return random.choice(string.ascii_uppercase)  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# # multiple apples
+# def spawn_and_get_apples(
+#     snake_body: list[tuple[int, int]],
+#     game_map,
+#     collected_letters: list[str],
+#     max_apples: int,
+# ) -> list[Apple]:
+#     apples = []
+
+#     # Store all positions that apples should not spawn on.
+#     occupied_positions = snake_body.copy()
+#     next_index = len(collected_letters)
+
+#     if next_index < len(TARGET_SEQUENCE):
+#         correct_letter = TARGET_SEQUENCE[next_index]
+#     else:
+#         correct_letter = "?"
+#     # Make sure there is at least one correct letter apple.
+#     correct_apple = Apple(
+#         game_map,
+#         correct_letter,
+#         occupied_positions,
+#     )
+#     # Make sure there is at least one correct letter apple.
+#     apples.append(correct_apple)
+#     occupied_positions.append(correct_apple.pos)
+#     # Spawn random letter apples.
+#     while len(apples) < max_apples:
+#         random_letter = get_random_apple_letter()
+#         random_apple = Apple(
+#             game_map,
+#             random_letter,
+#             occupied_positions,
+#         )
+
+#         apples.append(random_apple)
+#         occupied_positions.append(random_apple.pos)
+
+#     return apples
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 from settings import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
@@ -11,9 +10,7 @@ from settings import (
 from snake import Snake
 from apple import (
     Apple,
-    spawn_and_get_apples,
-    get_eaten_apple,
-    handle_apple_eaten,
+    AppleManager,
     check_target_completed,
 )
 from ui import UI
@@ -22,20 +19,21 @@ from events import handle_events
 
 
 # reset game
-def reset_game(game_map) -> tuple[Snake, list[Apple], list[str], bool, bool]:
+def reset_game(game_map) -> tuple[Snake, AppleManager, list[str], bool, bool]:
     player_snake = Snake()
     collected_letters: list[str] = []
 
-    apples = spawn_and_get_apples(
-        player_snake.body,
+    apple_manager = AppleManager(
+        player_snake,
         game_map,
         collected_letters,
         MAX_APPLES,
     )
+
     game_over = False
     game_win = False
 
-    return player_snake, apples, collected_letters, game_over, game_win
+    return player_snake, apple_manager, collected_letters, game_over, game_win
 
 
 def main():
@@ -58,18 +56,16 @@ def main():
     player_snake = Snake()
     # collected letters
     collected_letters = []
-    # apple
-    apples = spawn_and_get_apples(
-        player_snake.body,
+    # apple manager(apple)
+    apple_manager = AppleManager(
+        player_snake,
         game_map,
         collected_letters,
         MAX_APPLES,
     )
-    is_apple_eaten = False
 
     # main loop
     while game_running:
-
         # event handle (handle events like key press)
         game_running, player_snake.direction, restart_request = handle_events(
             game_running,
@@ -79,7 +75,7 @@ def main():
         if restart_request:
             (
                 player_snake,
-                apples,
+                apple_manager,
                 collected_letters,
                 game_over,
                 game_win,
@@ -87,7 +83,7 @@ def main():
         if not game_over and not game_win:
             # next_head_pos for checking whether the apple is eaten
             next_head = player_snake.get_next_head_pos()
-            eaten_apple = get_eaten_apple(next_head, apples)
+            eaten_apple = apple_manager.get_eaten_apple(next_head)
             if eaten_apple == None:
                 is_apple_eaten = False
             else:
@@ -99,12 +95,11 @@ def main():
                     game_over = True
                 else:
                     player_snake.revive()
-                    # respawn a new batch of apples
-                    apples = spawn_and_get_apples(
-                        player_snake.body,
+                    # spawn a new batch of apples
+                    apple_manager.spawn_apples(
+                        player_snake,
                         game_map,
                         collected_letters,
-                        MAX_APPLES,
                     )
             # self collision
             elif player_snake.check_self_collision(next_head, is_apple_eaten):
@@ -114,27 +109,22 @@ def main():
                 else:
                     player_snake.revive()
                     # respawn a new batch of apples
-                    apples = spawn_and_get_apples(
-                        player_snake.body,
+                    apple_manager.spawn_apples(
+                        player_snake,
                         game_map,
                         collected_letters,
-                        MAX_APPLES,
                     )
             # normal move
             else:
                 # include move and whether the snake should grow code function
                 player_snake.move(next_head, is_apple_eaten)
+                # include respawn apple
                 if is_apple_eaten:
-                    collected_letters = handle_apple_eaten(
+                    collected_letters = apple_manager.handle_apple_eaten(
                         eaten_apple,
-                        collected_letters,
-                    )
-                    # respawn a new batch of apples
-                    apples = spawn_and_get_apples(
-                        player_snake.body,
+                        player_snake,
                         game_map,
                         collected_letters,
-                        MAX_APPLES,
                     )
 
                     if check_target_completed(collected_letters):
@@ -144,8 +134,7 @@ def main():
         screen.fill(BACKGROUND_COLOUR)
         game_map.draw(screen)
         player_snake.draw(screen)
-        for apple in apples:
-            apple.draw(screen, font)
+        apple_manager.draw(screen, font)
         ui.draw(
             screen,
             player_snake.lives,
